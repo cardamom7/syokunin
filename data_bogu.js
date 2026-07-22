300de938d09f2bb1243a91f0958761cfb27abebc
cardamom7 <ayaka.yamaki.gene@gmail.com>
2026-07-23 02:41:01 +0900

    Update data_bogu.js

--- data_bogu.js
+++ data_bogu.js
@@ -1,1 +1,119 @@
-
+window.CRAFT_DATA["武器鍛冶職人"] = {
+  "片手剣": [
+    {
+      name: "輝天の剣",
+      level: "装備Lv 115",
+      materials: [
+        { name: "フォレスト鉱石", count: 30, shop: "バザー", defaultPrice: 14000 },
+        { name: "デモニウム鉱石", count: 15, shop: "バザー", defaultPrice: 8000 },
+        { name: "超ようせいのひだね", count: 6, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "セキバーンソード",
+      level: "装備Lv 110",
+      materials: [
+        { name: "プラチナ鉱石", count: 30, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "デモニウム鉱石", count: 12, shop: "バザー", defaultPrice: 8000 },
+        { name: "超ようせいのひだね", count: 5, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "サイレントアンサー",
+      level: "装備Lv 105",
+      materials: [
+        { name: "プラチナ鉱石", count: 25, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ゴールドストーン", count: 15, shop: "バザー", defaultPrice: 2000 },
+        { name: "ようせいのひだね", count: 12, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    }
+  ],
+  "両手剣": [
+    {
+      name: "輝天の大剣",
+      level: "装備Lv 115",
+      materials: [
+        { name: "フォレスト鉱石", count: 30, shop: "バザー", defaultPrice: 14000 },
+        { name: "ドラゴン骨", count: 15, shop: "バザー", defaultPrice: 5000 },
+        { name: "超ようせいのひだね", count: 6, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "ワイルドファング",
+      level: "装備Lv 110",
+      materials: [
+        { name: "プラチナ鉱石", count: 30, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ドラゴン骨", count: 12, shop: "バザー", defaultPrice: 5000 },
+        { name: "超ようせいのひだね", count: 5, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "ベルセルクソード",
+      level: "装備Lv 105",
+      materials: [
+        { name: "プラチナ鉱石", count: 25, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "大きな骨", count: 20, shop: "バザー", defaultPrice: 1500 },
+        { name: "ようせいのひだね", count: 12, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    }
+  ],
+  "オノ": [
+    {
+      name: "輝天のオノ",
+      level: "装備Lv 115",
+      materials: [
+        { name: "フォレスト鉱石", count: 30, shop: "バザー", defaultPrice: 14000 },
+        { name: "ヘビーメタル", count: 15, shop: "バザー", defaultPrice: 6000 },
+        { name: "超ようせいのひだね", count: 6, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "ブラッドレディ",
+      level: "装備Lv 110",
+      materials: [
+        { name: "プラチナ鉱石", count: 30, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ヘビーメタル", count: 12, shop: "バザー", defaultPrice: 6000 },
+        { name: "超ようせいのひだね", count: 5, shop: "バザー", defaultPrice: 32000 }
+      ]
+    }
+  ],
+  "鎌": [
+    {
+      name: "輝天の鎌",
+      level: "装備Lv 115",
+      materials: [
+        { name: "フォレスト鉱石", count: 30, shop: "バザー", defaultPrice: 14000 },
+        { name: "あやかしのつゆ", count: 15, shop: "バザー", defaultPrice: 4500 },
+        { name: "超ようせいのひだね", count: 6, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "デスストーカー",
+      level: "装備Lv 110",
+      materials: [
+        { name: "プラチナ鉱石", count: 30, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "あやかしのつゆ", count: 12, shop: "バザー", defaultPrice: 4500 },
+        { name: "超ようせいのひだね", count: 5, shop: "バザー", defaultPrice: 32000 }
+      ]
+    }
+  ],
+  "職人道具": [
+    {
+      name: "超 武器鍛冶ハンマー",
+      level: "職人Lv 40",
+      materials: [
+        { name: "プラチナ鉱石", count: 15, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ゴールドストーン", count: 8, shop: "バザー", defaultPrice: 2000 },
+        { name: "超ようせいのひだね", count: 2, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "光の武器鍛冶ハンマー",
+      level: "職人Lv 45",
+      materials: [
+        { name: "光のたね", count: 15, shop: "バザー", defaultPrice: 4000 },
+        { name: "超ようせいのひだね", count: 3, shop: "バザー", defaultPrice: 32000 }
+      ]
+    }
+  ]
+};
