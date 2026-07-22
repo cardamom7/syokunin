fdfca469d984c81c243d7d556c20ae539d973827
cardamom7 <ayaka.yamaki.gene@gmail.com>
2026-07-23 02:35:01 +0900

    Update data_tubo.js

--- data_tubo.js
+++ data_tubo.js
@@ -1,1 +1,109 @@
-
+window.CRAFT_DATA["ツボ錬金職人"] = {
+  "攻撃力": [
+    {
+      name: "攻撃力 +6 (成功)",
+      level: "錬金Lv 40",
+      materials: [
+        { name: "戦士の証", count: 2, shop: "バザー", defaultPrice: 15000 },
+        { name: "汗と涙の結晶", count: 10, shop: "バザー", defaultPrice: 3800 },
+        { name: "ようせいのひだね", count: 5, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    },
+    {
+      name: "攻撃力 +4 (成功)",
+      level: "錬金Lv 30",
+      materials: [
+        { name: "ちからのタネ", count: 1, shop: "バザー", defaultPrice: 5000 },
+        { name: "汗と涙の結晶", count: 5, shop: "バザー", defaultPrice: 3800 },
+        { name: "ようせいのひだね", count: 3, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    }
+  ],
+  "こうげき魔力": [
+    {
+      name: "こうげき魔力 +15 (成功)",
+      level: "錬金Lv 40",
+      materials: [
+        { name: "ダークマター", count: 2, shop: "バザー", defaultPrice: 18000 },
+        { name: "汗と涙の結晶", count: 10, shop: "バザー", defaultPrice: 3800 },
+        { name: "ようせいのひだね", count: 5, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    }
+  ],
+  "会心率": [
+    {
+      name: "かいしん率 +1.4% (成功)",
+      level: "錬金Lv 40",
+      materials: [
+        { name: "きごころのタネ", count: 2, shop: "バザー", defaultPrice: 12000 },
+        { name: "汗と涙の結晶", count: 10, shop: "バザー", defaultPrice: 3800 },
+        { name: "ようせいのひだね", count: 5, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 }
+      ]
+    }
+  ],
+  "守備力": [
+    {
+      name: "みのまもり +6 (成功)",
+      level: "錬金Lv 35",
+      materials: [
+        { name: "プラチナ鉱石", count: 10, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "汗と涙の結晶", count: 6, shop: "バザー", defaultPrice: 3800 }
+      ]
+    }
+  ],
+  "HP・MP": [
+    {
+      name: "さいだいHP +7 (成功)",
+      level: "錬金Lv 40",
+      materials: [
+        { name: "生命のきのみ", count: 2, shop: "バザー", defaultPrice: 16000 },
+        { name: "汗と涙の結晶", count: 10, shop: "バザー", defaultPrice: 3800 }
+      ]
+    },
+    {
+      name: "さいだいMP +15 (成功)",
+      level: "錬金Lv 40",
+      materials: [
+        { name: "まほうのせいすい", count: 5, shop: "両方", place: "各道具屋", defaultPrice: 500 },
+        { name: "汗と涙の結晶", count: 10, shop: "バザー", defaultPrice: 3800 }
+      ]
+    }
+  ],
+  "ガード率・重さ": [
+    {
+      name: "武器ガード率 +1.4% (成功)",
+      level: "錬金Lv 40",
+      materials: [
+        { name: "ヘビのぬけがら", count: 5, shop: "バザー", defaultPrice: 2000 },
+        { name: "汗と涙の結晶", count: 10, shop: "バザー", defaultPrice: 3800 }
+      ]
+    },
+    {
+      name: "おもり +15 (成功)",
+      level: "錬金Lv 40",
+      materials: [
+        { name: "ヘビーメタル", count: 3, shop: "バザー", defaultPrice: 6000 },
+        { name: "汗と涙の結晶", count: 10, shop: "バザー", defaultPrice: 3800 }
+      ]
+    }
+  ],
+  "職人道具": [
+    {
+      name: "超錬金ツボ",
+      level: "職人Lv 40",
+      materials: [
+        { name: "プラチナ鉱石", count: 20, shop: "両方", place: "住宅村素材屋", defaultPrice: 360 },
+        { name: "ようせいのひだね", count: 10, shop: "両方", place: "ギルド素材屋", defaultPrice: 1000 },
+        { name: "超ようせいのひだね", count: 2, shop: "バザー", defaultPrice: 32000 }
+      ]
+    },
+    {
+      name: "光の錬金ツボ",
+      level: "職人Lv 45",
+      materials: [
+        { name: "光のたね", count: 15, shop: "バザー", defaultPrice: 4000 },
+        { name: "超ようせいのひだね", count: 3, shop: "バザー", defaultPrice: 32000 }
+      ]
+    }
+  ]
+};
